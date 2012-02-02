69de8204402dcfffbe09559fa87798aa9a60d0b6
CyHamster <markymarkgrif@freezone.co.uk>
2012-02-02 18:56:07 +0000

    Update MAX31855.h

--- MAX31855.h
+++ MAX31855.h
@@ -7,30 +7,45 @@
 	#include "WProgram.h"
 #endif
 
-#define	FAULT_OPEN			10000
+
 #define	FAULT_SHORT_GND	10001
 #define	FAULT_SHORT_VCC	10002	
+#define	FAULT_OPEN	10003
+#define type_K 	20001
+#define type_J 	20002
+#define type_N 	20003
+#define type_S 	20004
+#define type_T 	20005
+#define type_E 	20006
+#define type_R 	20007
 
-enum	unit_t
+enum	scale
 {
-	CELSIUS,
-	FAHRENHEIT
+	FAHRENHEIT,
+	CELSIUS,	
+	VOLTAGE,
+	ADJUSTEDCELSIUS
 };
 
 class	MAX31855
 {
 	public:
-		MAX31855(unsigned char SO, unsigned	char CS, unsigned char SCK);
-	
-		double	readThermocouple(unit_t	unit);
-		double	readJunction(unit_t	unit);
+		MAX31855(byte SO, byte SCK, byte CS, int tcType);
+		
+		bool readData();
+		float temperature(scale unit);
+		float temperatureCJC(scale unit);
+		String faultCondition();
+		long bitString();
 		
 	private:
-		unsigned char so;
-		unsigned char cs;
-		unsigned char sck;
+		byte _sdo;
+		byte _cs;
+		byte _sck;
+		int _tc_type;
+		long bitData;
 		
-		unsigned long readData();
+		float adjustTemp(long inTemp);
 
 };
 #endif
